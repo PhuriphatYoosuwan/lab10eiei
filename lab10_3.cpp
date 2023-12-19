449cd2d8caea7bc94e66fc5b29e14a8fd1a5115e
PhuriphatYoosuwan <phuriphat_yoosuwan@cmu.ac.th>
2023-12-19 16:53:32 +0700

    done

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,23 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int n=0;
+    double sum=0;
+    double sum2=0;
+    string text;
+    ifstream source("score.txt");
+    while(getline(source,text)){
+    sum = sum+atof(text.c_str());
+    sum2 = sum2+pow(atof(text.c_str()),2);
+    n++;
+    }
+    
+    cout << "Number of data = " << n <<endl;
+    cout << setprecision(3);
+    cout << "Mean = " << sum/n <<endl;
+    cout << "Standard deviation = " << sqrt((1.0/n)*(sum2)-pow((sum/n),2));
+}
